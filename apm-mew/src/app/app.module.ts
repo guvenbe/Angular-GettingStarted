aef203e198369ed2b4c1c653ae4a29cf2dc5fb05
guvenbe <guvenbe@gmail.com>
2022-09-03 12:21:39 -0400

    all

--- apm-mew/src/app/app.module.ts
+++ apm-mew/src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductListComponent
-
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
